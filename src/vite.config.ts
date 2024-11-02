35d9d0183606523e08d281726d1dfae763b02e41
teckmill <teckmillion17@gmail.com>
2024-11-02 09:13:20 -0700

    Added compromise package and updated vite config

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This ensures assets are loaded correctly
+  base: '/',
+  build: {
+    rollupOptions: {
+      external: ['compromise'],
+    }
+  }
 }); 
